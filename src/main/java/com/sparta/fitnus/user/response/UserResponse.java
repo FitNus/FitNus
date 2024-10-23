f767b75284d377b58b00a15a62230f23cc8f5c75
tae98 <98rlaxogus@gmail.com>
2024-10-23 16:29:51 +0900

    fix: UserResponse누락 fix
    user/response/UserResponse 추가

---
+++ src/main/java/com/sparta/fitnus/user/response/UserResponse.java
@@ -0,0 +1,27 @@
+package com.sparta.fitnus.user.response;
+
+import com.sparta.fitnus.user.entity.User;
+import com.sparta.fitnus.user.enums.UserRole;
+import com.sparta.fitnus.user.enums.UserStatus;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserResponse {
+    private Long id;
+    private String email;
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+    private UserRole role;
+    private UserStatus status;
+
+    public UserResponse(User user) {
+        this.id = user.getUser_id();
+        this.email = user.getEmail();
+        this.createAt = user.getCreatedAt();
+        this.modifiedAt = user.getModifiedAt();
+        this.role = user.getUserRole();
+        this.status = user.getStatus();
+    }
+}
