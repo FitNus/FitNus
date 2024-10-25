d9efda973d7c6cfbba69806d66d5c3e71eb00323
ilmechaJu <pawor006@naver.com>
2024-10-25 11:26:13 +0900

    feat : CRUD-CenterReview complete.

--- src/main/java/com/sparta/fitnus/review/center/entity/CenterReview.java
+++ src/main/java/com/sparta/fitnus/review/center/entity/CenterReview.java
@@ -1,6 +1,7 @@
 package com.sparta.fitnus.review.center.entity;
 
 import com.sparta.fitnus.center.entity.Center;
+import com.sparta.fitnus.review.center.dto.request.CenterReviewRequest;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,28 @@ public class CenterReview {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long userId;
+
+    private String writerNickname;
+
+    private int centerScore;
+
+    private String comment;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "center_id")
     private Center center;
+
+    public CenterReview(CenterReviewRequest request, Center center) {
+        this.userId = request.getUserId();
+        this.writerNickname = request.getWriterNickName();
+        this.centerScore = request.getCenterScore();
+        this.comment = request.getComment();
+        this.center = center;
+    }
+
+    public static CenterReview of(CenterReviewRequest request, Center center) {
+        return new CenterReview(request, center);
+    }
+
 }
