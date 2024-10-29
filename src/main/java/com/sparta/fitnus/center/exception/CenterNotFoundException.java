bfab8b661d6d06de97f4ed01d51ac55a3096bd0f
ilmechaJu <pawor006@naver.com>
2024-10-30 01:05:54 +0900

    Refactor : Make CenterNotFoundException

---
+++ src/main/java/com/sparta/fitnus/center/exception/CenterNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sparta.fitnus.center.exception;
+
+public class CenterNotFoundException extends RuntimeException {
+  public CenterNotFoundException(String message) {
+    super(message);
+  }
+}
