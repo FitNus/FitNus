0265beadcf269218c55afcd6b8168e3bd343e680
Your Name <doolchong@gmail.com>
2024-11-04 23:07:40 +0900

    refactor: 정산 기능 스케쥴러 적용

--- module-batch/src/main/java/com/sparta/modulebatch/batch/settlementBatch.java
+++ module-batch/src/main/java/com/sparta/modulebatch/batch/settlementBatch.java
@@ -20,6 +20,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Map;
 
 @Configuration
@@ -44,20 +48,29 @@ public class settlementBatch {
     @Bean
     public Step settlementFirstStep() {
         return new StepBuilder("settlementFirstStep", jobRepository)
-                .<SettlementResult, Result>chunk(10, platformTransactionManager)
+                .<SettlementResult, Result>chunk(100, platformTransactionManager)
                 .reader(settlementReader())
                 .processor(settlementProcessor())
                 .writer(settlementWriter())
+                .faultTolerant() // 내결함성 설정
+                .skipLimit(5) // 스킵할 최대 항목 수
+                .skip(Exception.class) // 특정 예외에 대해 스킵
+                .retryLimit(3) // 재시도 횟수 설정
+                .retry(Exception.class) // 특정 예외에 대해 재시도
                 .build();
     }
 
     @Bean
     public RepositoryItemReader<SettlementResult> settlementReader() {
+        LocalDate now = LocalDate.now().minusDays(1);
+        LocalDateTime startDateTime = now.with(TemporalAdjusters.firstDayOfMonth()).atTime(LocalTime.MIN);
+        LocalDateTime endDateTime = now.with(TemporalAdjusters.lastDayOfMonth()).atTime(LocalTime.MAX);
         return new RepositoryItemReaderBuilder<SettlementResult>()
                 .name("settlementReader")
-                .pageSize(10)
+                .pageSize(100)
                 .methodName("findAllByTimeslotIdAndRequiredCoupon")
                 .repository(scheduleRepository)
+                .arguments(startDateTime, endDateTime)
                 .sorts(Map.of("c.id", Sort.Direction.ASC))
                 .build();
     }
@@ -67,8 +80,9 @@ public class settlementBatch {
         return new ItemProcessor<SettlementResult, Result>() {
 
             @Override
-            public Result process(SettlementResult item) throws Exception {
-                return Result.of(item);
+            public Result process(SettlementResult item) {
+                // 수수료 5%
+                return Result.of(item.getCenterId(), item.getSumOfCoupon() * 1000 * 0.95);
             }
         };
     }
--- module-common/src/test/java/com/sparta/modulecommon/testdata/schedule/ScheduleDummyDataTest.java
+++ module-common/src/test/java/com/sparta/modulecommon/testdata/schedule/ScheduleDummyDataTest.java
@@ -4,21 +4,18 @@
 //import com.sparta.modulecommon.center.repository.CenterBulkRepository;
 //import com.sparta.modulecommon.fitness.entity.Fitness;
 //import com.sparta.modulecommon.fitness.repository.FitnessBulkRepository;
-//import com.sparta.modulecommon.schedule.entity.Schedule;
 //import com.sparta.modulecommon.schedule.repository.ScheduleBulkRepository;
-//import com.sparta.modulecommon.timeslot.entity.Timeslot;
 //import com.sparta.modulecommon.timeslot.repository.TimeslotBulkRepository;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.util.ReflectionTestUtils;
 //
-//import java.time.LocalDateTime;
 //import java.util.ArrayList;
 //import java.util.List;
 //
 //@SpringBootTest
-//public class ScheduleDummyData {
+//public class ScheduleDummyDataTest {
 //
 //    @Autowired
 //    private CenterBulkRepository centerBulkRepository;
@@ -61,42 +58,42 @@
 //
 //        fitnessBulkRepository.saveAll(fitnessList);
 //
-//        for (int i = 1; i <= 30; i++) {
-//            List<Timeslot> timeslotList = new ArrayList<>();
-//            LocalDateTime startTime = LocalDateTime.now();
-//
-//            for (int j = 0; j < 500; j++) {
-//                Fitness fitness = new Fitness();
-//                ReflectionTestUtils.setField(fitness, "id", (long) j + 1);
-//                Timeslot timeslot = new Timeslot();
-//                ReflectionTestUtils.setField(timeslot, "fitness", fitness);
-//                ReflectionTestUtils.setField(timeslot, "startTime", startTime);
-//
-//                timeslotList.add(timeslot);
-//            }
-//
-//            timeslotBulkRepository.saveAll(timeslotList);
-//        }
-//
-//        for (int i = 1; i <= 30; i++) {
-//            for (int j = 1; j <= 500; j++) {
-//                Timeslot timeslot = new Timeslot();
-//                ReflectionTestUtils.setField(timeslot, "id", (long) j);
-//                List<Schedule> scheduleList = new ArrayList<>();
-//
-//                for (int k = 1; k <= 500; k++) {
-//                    Schedule schedule = new Schedule();
-//                    ReflectionTestUtils.setField(schedule, "userId", (long) k);
-//                    ReflectionTestUtils.setField(schedule, "startTime", LocalDateTime.now().plusDays(i - 1));
-//                    ReflectionTestUtils.setField(schedule, "requiredCoupon", 3);
-//                    ReflectionTestUtils.setField(schedule, "timeslotId", timeslot.getId());
-//
-//                    scheduleList.add(schedule);
-//                }
-//
-//                scheduleBulkRepository.saveAll(scheduleList);
-//            }
-//        }
+////        for (int i = 1; i <= 30; i++) {
+////            List<Timeslot> timeslotList = new ArrayList<>();
+////            LocalDateTime startTime = LocalDateTime.now();
+////
+////            for (int j = 0; j < 500; j++) {
+////                Fitness fitness = new Fitness();
+////                ReflectionTestUtils.setField(fitness, "id", (long) j + 1);
+////                Timeslot timeslot = new Timeslot();
+////                ReflectionTestUtils.setField(timeslot, "fitness", fitness);
+////                ReflectionTestUtils.setField(timeslot, "startTime", startTime);
+////
+////                timeslotList.add(timeslot);
+////            }
+////
+////            timeslotBulkRepository.saveAll(timeslotList);
+////        }
+////
+////        for (int i = 1; i <= 30; i++) {
+////            for (int j = 1; j <= 500; j++) {
+////                Timeslot timeslot = new Timeslot();
+////                ReflectionTestUtils.setField(timeslot, "id", (long) j);
+////                List<Schedule> scheduleList = new ArrayList<>();
+////
+////                for (int k = 1; k <= 500; k++) {
+////                    Schedule schedule = new Schedule();
+////                    ReflectionTestUtils.setField(schedule, "userId", (long) k);
+////                    ReflectionTestUtils.setField(schedule, "startTime", LocalDateTime.now().plusDays(i - 1));
+////                    ReflectionTestUtils.setField(schedule, "requiredCoupon", 3);
+////                    ReflectionTestUtils.setField(schedule, "timeslotId", timeslot.getId());
+////
+////                    scheduleList.add(schedule);
+////                }
+////
+////                scheduleBulkRepository.saveAll(scheduleList);
+////            }
+////        }
 //    }
 //
 ////    @Test
