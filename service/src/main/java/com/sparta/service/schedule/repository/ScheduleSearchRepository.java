5b923d218b5491c24cd05d1ce8682426048cbccd
pookyspooky <union0802@naver.com>
2024-11-17 04:10:08 +0900

    refactor: ScheduleSearchRepository 수정

--- service/src/main/java/com/sparta/service/schedule/repository/ScheduleSearchRepository.java
+++ service/src/main/java/com/sparta/service/schedule/repository/ScheduleSearchRepository.java
@@ -1,13 +1,8 @@
 package com.sparta.service.schedule.repository;
 
 import com.sparta.service.schedule.entity.ScheduleSearch;
-import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ScheduleSearchRepository extends ElasticsearchRepository<ScheduleSearch, Long> {
 
-    List<ScheduleSearch> findByUserIdAndYearAndMonthAndDay(Long userId, Integer year, Integer month,
-            Integer day);
-
-    List<ScheduleSearch> findByUserIdAndYearAndMonth(Long userId, Integer year, Integer month);
 }
