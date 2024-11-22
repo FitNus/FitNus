2c9ec34bbe51f3be99bbc072855e2f2630335184
Your Name <doolchong@gmail.com>
2024-11-22 10:54:26 +0900

    fix: 배치 오류 수정

---
+++ batch/src/main/java/com/sparta/batch/entity/Center.java
@@ -0,0 +1,49 @@
+package com.sparta.batch.entity;
+
+import com.sparta.common.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.geo.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "center")
+public class Center extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //센터장 id
+    @Column(name = "owner_id")
+    private Long ownerId;
+
+    @Column(name = "center_name")
+    private String centerName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "open_time")
+    private Integer openTime;
+
+    @Column(name = "close_time")
+    private Integer closeTime;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "location")
+    private Point location;
+
+    @OneToMany(mappedBy = "center", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Fitness> fitnesses = new ArrayList<>();
+}
